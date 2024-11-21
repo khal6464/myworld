07e0e66fb30e048468ea4f265f5df88ac5e56b19
khal6464 <khaldrogo9810@gmail.com>
2024-11-21 18:35:43 +0530

    Update settings.py

--- naman/naman/settings.py
+++ naman/naman/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u%$7_v%8q7r!d0-of0-#sx&l&$fgj1br9s!qyl*wz2+@h*=o^#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
